30c350f0bc98725270b9afc6f61cd372e0474bc1
GanapathiV2003 <146443987+GanapathiV2003@users.noreply.github.com>
2025-01-24 18:53:04 +0530

    add : added a new line

--- firstprogram.java
+++ firstprogram.java
@@ -2,6 +2,7 @@ class firstprogram
 {
     public static void main(String args[]){
         System.out.println("this is my first program");
+        System.out.println("have a nice day");
     }
     
 }
